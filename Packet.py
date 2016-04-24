b5f0416e08f9b58016396052e17bb023b7ee20fc
Ivaness <ichiong@hawk.iit.edu>
2016-04-24 13:00:21 -0700

    updated file

--- Packet.py
+++ Packet.py
@@ -14,7 +14,12 @@ class Packet:
 			self.dest = socket.inet_aton(fields[1])
                         self.prot = int(fields[2]) 
                         self.timestamp = float(fields[3])
-			self.size = int(fields[4])
+                        try:
+                                self.size = int(fields[4])
+                        except IndexError:
+                                self.size = 0
+                        else:
+                                self.size = int(fields[4])
 			if self.source < self.dest:
 				self.key = self.source + self.dest
 			else:
